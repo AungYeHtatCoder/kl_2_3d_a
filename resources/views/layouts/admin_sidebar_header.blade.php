d7d8204c482a8c0b5657d8b24c3f1c37a697ee3f
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-02-14 09:58:40 +0630

    update 2s

--- resources/views/layouts/admin_sidebar_header.blade.php
+++ resources/views/layouts/admin_sidebar_header.blade.php
@@ -1,8 +1,8 @@
  <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="{{ url('/') }}">
-        <img src="{{ asset('user_app/assets/images/shwe_sin_logo.jpg') }}" class="navbar-brand-img h-100" alt="main_logo">
-        <span class="ms-1 font-weight-bold text-white">Shwe Sin 2D 3D</span>
+        <img src="{{ asset('user_app/assets/images/kl_logo.png') }}" class="navbar-brand-img h-100" alt="main_logo">
+        <span class="ms-1 font-weight-bold text-white">KL 2D 3D</span>
       </a>
     </div>
     {{-- <link rel="icon" href="{{ asset('user_app/assets/images/Logo RED PNG.png') }}"> --}}

568c8db21d19e0783d523739e78875de9ffdde1e
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-02-14 09:08:44 +0630

    first

---
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -0,0 +1,16 @@
+<div class="d-flex justify-content-between align-items-center">
+    <div>
+
+      <img src="{{ asset('user_app/assets/images/shwe_sin_logo.jpg') }}" alt="logo" style="width: 60px; height: 60px;"/>
+    </div>
+      
+      <h5 class="d-inline-block">
+          <a href="{{ url('/') }}" class="text-decoration-none"
+          >Shwe Sin 2D | 3D</a
+          >
+      </h5>
+      <span>
+          <button class="material-icons text-decoration-none btn" style="color:#f5bd02; border:none" onclick="refreshPage()"
+            >refresh</button>   
+      </span>
+  </div>
